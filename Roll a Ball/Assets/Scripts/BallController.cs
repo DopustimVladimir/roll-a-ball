d8fdacf84ef675eda21ad81cfd462456b8242180
Vladimir <dopustimvladimir@gmail.com>
2018-12-19 05:32:25 +0600

    Make the Ball interactive

---
+++ Roll a Ball/Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 50f;
+
+    private Rigidbody rb;
+    private float moveH;
+    private float moveV;
+
+    private Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveH, 0.0f, moveV);
+
+        rb.AddForce(movement * speed);
+    }
+}
