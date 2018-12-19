4bf574a6079a2cf58c50fc47786f747b74685213
Vladimir <dopustimvladimir@gmail.com>
2018-12-19 06:09:46 +0600

    Let Camera follow the Ball

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
