e6d5eb240d51a0105c2cbe0eafcff161bb105a9f
Vladimir <dopustimvladimir@gmail.com>
2018-12-19 06:58:18 +0600

    Create some animated Things

---
+++ Roll a Ball/Assets/Scripts/BladesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladesController : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+
+    private Vector3 rotation;
+
+    void Start()
+    {
+        rotation = new Vector3(180, 0, 0);
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotation * rotationSpeed * Time.deltaTime);
+    }
+}
